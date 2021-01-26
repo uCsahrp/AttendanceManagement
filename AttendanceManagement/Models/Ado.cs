84d63a24411530ff4b8be4277ff6a921bf7bc0eb
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-01-26 01:22:39 +0100

    Fixes

--- AttendanceManagement/Models/Ado.cs
+++ AttendanceManagement/Models/Ado.cs
@@ -44,7 +44,7 @@ namespace AttendanceManagement.Models
         {
             if (Cnx.State == ConnectionState.Closed || Cnx.State == ConnectionState.Broken)
             {
-                Cnx.ConnectionString = "DataSource=ADAM-DELL;Initial Catalog=AttendanceManagement;Integrated Security=True";
+                Cnx.ConnectionString = "Data Source=ADAM-HP;Initial Catalog=AttendanceManagement;Integrated Security=True";
                 Cnx.Open();
             }
         }
