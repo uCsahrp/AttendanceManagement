67d099b125dc8da3977f168a73efa55801077d75
Majda FANNAN <fannanmajda123@gmail.com>
2021-01-27 14:19:50 +0100

    Staf

--- AttendanceManagement/Views/Staff.xaml.cs
+++ AttendanceManagement/Views/Staff.xaml.cs
@@ -1,32 +1,203 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
+//using System.Data;
+//using System.Data.SqlClient;
 
-namespace AttendanceManagement.Views
-{
-    /// <summary>
-    /// Logique d'interaction pour Staff.xaml
-    /// </summary>
-    public partial class Staff : Window
-    {
-        public Staff()
-        {
-            InitializeComponent();
-        }
+//namespace AttendanceManagement.Views
+//{
+//    /// <summary>
+//    /// Logique d'interaction pour Staff.xaml
+//    /// </summary>
+//    public partial class Staff : Window
+//    {
+//        public Staff()
+//        {
+//            InitializeComponent();
+//        }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
+//        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-OC51QH5;Initial Catalog=AttendanceManagement;Integrated Security=True");
+//        SqlCommand Cmd;
+//        SqlDataAdapter Sda;
+//        SqlDataReader dr;
+//        DataSet ds = new DataSet();
+//        DataTable dt = new DataTable();
 
-        }
-    }
-}
+//        public string date = DateTime.Now.ToString("dd-MM-yyyy");
+
+
+
+
+//        DataTable Dt = new DataTable();
+
+//        private void Grid_Loaded(object sender, RoutedEventArgs e)
+//        {
+//            dg.Visibility = Visibility.Visible;
+//            dg1.Visibility = Visibility.Hidden;
+//            conn.Open();
+
+//            Cmd = new SqlCommand("select u.[User Id],u.[Full Name],u.Email,c.[Class Name] from Users u inner join Classes c on u.[Class Id]=c.[Id Class] where u.[Role Id]=4", conn);
+//            SqlDataReader dr = Cmd.ExecuteReader();
+//            DataTable t = new DataTable();
+//            t.Load(dr);
+//            dg.ItemsSource = t.DefaultView;
+//            dr.Close();
+
+//            conn.Close();
+
+//            late_temp.Visibility = Visibility.Hidden;
+//            abs_temp.Visibility = Visibility.Hidden;
+
+//        }
+
+//        private void btn_ajouter_ab_Click(object sender, RoutedEventArgs e)
+//        {
+//            dg.Visibility = Visibility.Visible;
+//            dg1.Visibility = Visibility.Hidden;
+//            late_temp.Visibility = Visibility.Visible;
+//            abs_temp.Visibility = Visibility.Visible;
+
+//        }
+
+
+
+//        private void check_absent_unchecked(object sender, RoutedEventArgs e)
+//        {
+
+//            DataRowView row = dg.SelectedItem as DataRowView;
+//            int id_student = Convert.ToInt32(row.Row[0].ToString());
+//            conn.Open();
+//            SqlCommand cmd1 = new SqlCommand("delete from Attendance where [Student id]='" + id_student + "' and Date = '" + date + "' and Absent = 'oui'", conn);
+//            cmd1.ExecuteNonQuery();
+//            conn.Close();
+
+//        }
+
+
+
+//        private void check_retard_checked(object sender, RoutedEventArgs e)
+//        {
+//            check_absent_unchecked(sender, e);
+//            DataRowView row = dg.SelectedItem as DataRowView;
+//            int id_student = Convert.ToInt32(row.Row[0].ToString());
+//            conn.Open();
+//            SqlCommand cmd1 = new SqlCommand("INSERT INTO Attendance (Date, IsJustified, [Student id],Absent,Retard) VALUES ('" + date + "','false','" + id_student + "', 'non' , 'oui')", conn);
+//            cmd1.ExecuteNonQuery();
+//            conn.Close();
+//        }
+
+//        private void check_retard_unchecked(object sender, RoutedEventArgs e)
+//        {
+//            DataRowView row = dg.SelectedItem as DataRowView;
+//            int id_student = Convert.ToInt32(row.Row[0].ToString());
+//            conn.Open();
+//            SqlCommand cmd1 = new SqlCommand("delete from Attendance where [Student id]='" + id_student + "' and Date = '" + date + "'and retard = 'oui'", conn);
+//            cmd1.ExecuteNonQuery();
+//            conn.Close();
+//        }
+
+//        private void afficher_les_absents_Click(object sender, RoutedEventArgs e)
+//        {
+
+//            if (conn.State == ConnectionState.Closed)
+//            {
+//                conn.Open();
+//                dg.Visibility = Visibility.Hidden;
+//                dg1.Visibility = Visibility.Visible;
+
+
+//                Cmd = new SqlCommand("select a.[Date] , u.[Full Name] , a.[Description] ,c.[Class Name] From Users u inner join Attendance a on a.[Student Id] =u.[User Id] inner join Classes c on c.[Id Class]=u.[Class Id]", conn);
+//                SqlDataReader dr = Cmd.ExecuteReader();
+//                DataTable t = new DataTable();
+//                t.Load(dr);
+//                dr.Close();
+//                dg1.ItemsSource = t.DefaultView;
+//            }
+//            else
+//            {
+//                dg.Visibility = Visibility.Hidden;
+//                dg1.Visibility = Visibility.Visible;
+
+
+//                Cmd = new SqlCommand("select u.[User Id],u.[Full Name],u.Email,c.[Class Name] from Users u inner join Classes c on u.[Class Id]=c.[Id Class] where u.[Role Id]=4", conn);
+//                SqlDataReader dr = Cmd.ExecuteReader();
+//                DataTable t = new DataTable();
+//                t.Load(dr);
+//                dr.Close();
+//                dg1.ItemsSource = t.DefaultView;
+//            }
+
+
+
+
+//        }
+
+//        private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+
+//        }
+
+//        private void check_absent_checked(object sender, RoutedEventArgs e)
+//        {
+//            check_retard_unchecked(sender, e);
+//            DataRowView row = dg.SelectedItem as DataRowView;
+//            int id_student = Convert.ToInt32(row.Row[0].ToString());
+//            conn.Open();
+//            SqlCommand cmd1 = new SqlCommand("INSERT INTO Attendance (Date, IsJustified, [Student id],Absent,Retard) VALUES ('" + date + "','false','" + id_student + "', 'oui' , 'non')", conn);
+//            cmd1.ExecuteNonQuery();
+//            conn.Close();
+//        }
+
+//        private void text_search_TextChanged(object sender, TextChangedEventArgs e)
+//        {
+//            if (conn.State == ConnectionState.Closed)
+//            {
+//                conn.Open();
+//                dg.Visibility = Visibility.Visible;
+//                dg1.Visibility = Visibility.Hidden;
+//                late_temp.Visibility = Visibility.Visible;
+//                abs_temp.Visibility = Visibility.Visible;
+//                string text = text_search.Text;
+//                Cmd = new SqlCommand("select u.[User Id],u.[Full Name],u.Email,c.[Class Name] from Users u inner join Classes c on u.[Class Id]=c.[Id Class] where u.[Role Id]=4 and u.[Full Name] like '%" + text + "%' ", conn);
+//                SqlDataReader dr = Cmd.ExecuteReader();
+//                DataTable t = new DataTable();
+//                t.Load(dr);
+//                dg.ItemsSource = t.DefaultView;
+//                dr.Close();
+
+//                conn.Close();
+//            }
+//            else
+//            {
+//                dg.Visibility = Visibility.Visible;
+//                dg1.Visibility = Visibility.Hidden;
+//                late_temp.Visibility = Visibility.Visible;
+//                abs_temp.Visibility = Visibility.Visible;
+//                string text = text_search.Text;
+//                Cmd = new SqlCommand("select u.[User Id],u.[Full Name],u.Email,c.[Class Name] from Users u inner join Classes c on u.[Class Id]=c.[Id Class] where u.[Role Id]=4 and u.[Full Name] like '%" + text + "%' ", conn);
+//                SqlDataReader dr = Cmd.ExecuteReader();
+//                DataTable t = new DataTable();
+//                t.Load(dr);
+//                dg.ItemsSource = t.DefaultView;
+//                dr.Close();
+
+//                conn.Close();
+//            }
+
+
+
+//        }
+//    }
+//}
+//    }
+//}
