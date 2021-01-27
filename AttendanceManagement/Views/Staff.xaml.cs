1eece5f139e2c8a50182b439284004db879ece4e
Majda FANNAN <fannanmajda123@gmail.com>
2021-01-27 11:53:34 +0100

    Staff  in progress

---
+++ AttendanceManagement/Views/Staff.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AttendanceManagement.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Staff.xaml
+    /// </summary>
+    public partial class Staff : Window
+    {
+        public Staff()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
